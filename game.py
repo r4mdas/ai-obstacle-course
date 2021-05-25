6d79bf33d1e720db385ae9f08db31507d1f3cb81
Ramdas Nair <ramdasnairhs@gmail.com>
2021-05-26 00:03:58 +0530

    Added config file. Generations HUD and NEAT simulation, pending fitness function completion.

--- game.py
+++ game.py
@@ -1,16 +1,33 @@
 import pygame
 from creature import Creature
 from obstacle import Obstacle
+import neat
 
+import os
 import sys
 
 WIDTH = 1280
 HEIGHT = 720
 
 CAPTION = "AI Obstacle Course"
+gen = 0
+
+pygame.font.init()
+STAT_FONT = pygame.font.SysFont("comicsans", 50)
 
 
-def game_start():
+def draw_hud(screen):
+    # generations
+    global STAT_FONT
+    score_label = STAT_FONT.render("Generations: " + str(gen - 1), 1, (255, 255, 255))
+    screen.blit(score_label, (10, 10))
+
+
+def game_start(genomes, config):
+
+    global gen
+
+    gen += 1
     pygame.init()
     pygame.display.set_caption(CAPTION)
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -19,12 +36,23 @@ def game_start():
     game_surf = pygame.Surface((WIDTH, HEIGHT))
     game_surf.fill((200, 255, 0))
 
-    c1 = Creature(WIDTH/2, 600, 15)
+    nets = []
+    creatures = []
+    ge = []
+
+    for genome_id, genome in genomes:
+        genome.fitness = 0  # start with fitness level of 0
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        nets.append(net)
+        creatures.append(Creature(WIDTH / 2, 600, 15))
+        ge.append(genome)
+
+    c1 = Creature(WIDTH / 2, 600, 15)
 
     obs = []
-    obstacle_1 = Obstacle(0, 200, WIDTH-400, 15)
+    obstacle_1 = Obstacle(0, 200, WIDTH - 400, 15)
     obs.append(obstacle_1)
-    obstacle_2 = Obstacle(WIDTH-600, 300, 600, 15)
+    obstacle_2 = Obstacle(WIDTH - 600, 300, 600, 15)
     obs.append(obstacle_2)
 
     while True:
@@ -66,9 +94,30 @@ def game_start():
         if c1.collision:
             c1 = Creature(WIDTH / 2, 600, 15)
 
+        draw_hud(screen)
         pygame.display.flip()
         clock.tick(60)
 
 
-if __name__ == "__main__":
-    game_start()
+def run(config_file):
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                                neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                                config_file)
+    p = neat.Population(config)
+
+    # Add a stdout reporter to show progress in the terminal.
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    # Run for up to 50 generations.
+    winner = p.run(game_start, 50)
+
+    # show final stats
+    print('\nBest genome:\n{!s}'.format(winner))
+
+
+if __name__ == '__main__':
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'neat-config.txt')
+    run(config_path)
